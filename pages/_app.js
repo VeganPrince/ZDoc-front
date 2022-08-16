02618b671954e451f35d213656c35d7fab18e837
Alexandre Dantas <alexandre.dantas0013@gmail.com>
2022-08-16 07:24:17 -0300

    add new componentes and the styles

--- pages/_app.js
+++ pages/_app.js
@@ -2,9 +2,6 @@ import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
-  useEffect(()=>{
-    import("bootstrap/dist/js/bootstrap");
-},[])
 }
 
 export default MyApp
